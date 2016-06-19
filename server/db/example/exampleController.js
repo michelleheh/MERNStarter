bb5718cef34059437509594c620c6ed136741e92
michelleheh <xiaoyu.he22@gmail.com>
2016-06-18 22:54:19 -0700

    Setup exampleController.js

--- server/db/example/exampleController.js
+++ server/db/example/exampleController.js
@@ -1,12 +1,30 @@
-const Example = require('./exampleModel.js');
+const Promise = require("bluebird");
+const Example = Promise.promisifyAll(require('./exampleModel.js'));
 
 module.exports = {
   findAll(req, res) {
-    console.log('findAll fired!')
-    res.send('OK');
+    Example.find({})
+      .then((results) => {
+        console.log('results sent in finaAll!')
+        res.send(results);
+      })
+      .catch((err) => {
+        res.status(500).send(err);
+      });
   },
   addOne(req, res) {
-    console.log('addOne fired!')
-    res.send('OK');
+    // req.body example
+    // {
+    //   "name":"Michelle",
+    //   "count":1
+    // }
+    const entry = new Example(req.body);
+    entry.save((err, model) => {
+      if (err) {
+        console.log('Error in addOne!');
+        res.status(500).send(err);
+      }
+      res.send(model);
+    });
   }
 };
